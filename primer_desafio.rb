255a4cc37759a5c08ac4392118c98a185d227173
NelsonAlejandroHerreraGomez <ingnelsonherrera@gmail.com>
2023-04-01 21:45:01 -0300

    primer desafio RubyDIO

---
+++ primer_desafio.rb
@@ -0,0 +1,9 @@
+# DESAFIO INICIAL
+print " Bem vindo ao Museu ....digite seu nome:  "
+nome = gets.chomp
+print " digite seu sobrenome :mNW1"
+sobrenome = gets.chomp
+print " Sua idade é "
+idade = gets.chomp.to_i
+
+puts "Seja bem vindo #{nome} #{sobrenome} . Vc é parte das pessoas da #{idade} qual tem um desconto na entrada"
